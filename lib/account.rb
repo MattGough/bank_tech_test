502bd17f1034213d905bb469cceac6cab0b4e74d
Matt <mattpgough@gmail.com>
2016-04-06 11:58:47 +0100

    Passed test for printing last transaction, refactored code to have some private methods and removed redundant test.

--- lib/account.rb
+++ lib/account.rb
@@ -1,20 +1,46 @@
+require 'date'
+
 class Account
 
   START_BALANCE = 0
   OVERDRAFT_LIMIT = 0
+  DATE_TODAY = Date.today.to_s
 
   attr_reader :balance
 
   def initialize
     @balance = START_BALANCE
+    @transactions = []
   end
 
   def deposit(amount)
-    @balance += amount
+    increase_balance(amount)
+    save_transaction("IN", amount)
   end
 
   def withdraw(amount)
-    raise "Insufficient funds: balance is #{@balance}" if @balance - amount <= OVERDRAFT_LIMIT
+    error_message = "Insufficient funds: balance is £#{@balance}"
+    raise error_message if @balance - amount <= OVERDRAFT_LIMIT
+    decrease_balance(amount)
+    save_transaction("OUT", amount)
+  end
+
+  def print_last_transaction
+    tran = @transactions.last
+    "#{tran[0]}, Date: #{tran[1]}, Amount: £#{tran[2]}, Balance: £#{tran[3]}"
+  end
+
+  private
+
+  def increase_balance(amount)
+    @balance += amount
+  end
+
+  def decrease_balance(amount)
     @balance -= amount
+  end
+
+  def save_transaction(payment_type, amount)
+    @transactions << [payment_type, DATE_TODAY, amount , @balance]
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,8 +3,6 @@ require './lib/account'
 describe Account do
   subject(:account) { described_class.new }
 
-  it { is_expected.to respond_to(:deposit).with(1).argument }
-
   it 'should have a starting balance of zero' do
     expect(account.balance).to eq(0)
   end
@@ -21,6 +19,11 @@ describe Account do
   end
 
   it 'should stop the user withdrawing money if none is avaialble' do
-    expect{ account.withdraw(50) }.to raise_error "Insufficient funds: balance is 0"
+    expect{ account.withdraw(50) }.to raise_error "Insufficient funds: balance is £0"
+  end
+
+  it 'should print a transaction statememt with date, amount & balance' do
+    account.deposit(50)
+    expect(account.print_last_transaction).to eq("IN, Date: #{Date.today.to_s}, Amount: £50, Balance: £50")
   end
 end
